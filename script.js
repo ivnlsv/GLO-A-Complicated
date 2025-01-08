a2b2a6ede9253ef373278a8966428ba88fd5f117
ivnlsv <ivan.v.losev@gmail.com>
2025-01-08 12:13:34 +0300

    Update

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-let arr = ["245", "456987", "57643", "89017623", "213654", "1234098", "7653"];
-let filteredNumbers = arr.filter(num => num.startsWith("2") || num.startsWith("4"));
-console.log("Числа, начинающиеся с 2 или 4:", filteredNumbers);
+const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
 
-for (let n = 1; n <= 100; n++) {
-    let isSimple = true;
-  
-    if (n > 1) {
-      for (let i = 2; i < n; i++) {
-        if (n % i === 0) {
-          isSimple = false;
-          break;
-        }
-      }
+const today = new Date();
+const currentDay = today.getDay(); 
+const currentDayIndex = currentDay === 0 ? 6 : currentDay - 1; 
+
+week.forEach((day, index) => {
+    if (index === 5 || index === 6) { 
+        console.log(`%c${day}`, 'font-style: italic;');
+    } else if (index === currentDayIndex) {
+        console.log(`%c${day}`, 'font-weight: bold;');
     } else {
-      isSimple = false; 
+        console.log(day);
     }
+});
 
-    if (isSimple) {
-      console.log(`${n} — Делители числа: 1 и ${n}`);
-    }
-  }
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
