f89e9b476aeb96b92711176561f2e49e4cc093d5
ivnlsv <78484056+ivnlsv@users.noreply.github.com>
2025-01-08 12:18:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
 
-const today = new Date();
-const currentDay = today.getDay(); 
-const currentDayIndex = currentDay === 0 ? 6 : currentDay - 1; 
-
-week.forEach((day, index) => {
-    if (index === 5 || index === 6) { 
-        console.log(`%c${day}`, 'font-style: italic;');
-    } else if (index === currentDayIndex) {
-        console.log(`%c${day}`, 'font-weight: bold;');
-    } else {
-        console.log(day);
-    }
-});
