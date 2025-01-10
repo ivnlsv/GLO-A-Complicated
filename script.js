44285e8f57d517645b55b0562a9c8d2aa756f52b
ivnlsv <ivan.losev@denuo.legal>
2025-01-10 12:43:16 +0300

    updated

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+function getHourWord(hour) {
+  if (hour % 10 === 1 && hour % 100 !== 11) {
+      return 'час';
+  } else if ((hour % 10 >= 2 && hour % 10 <= 4) && (hour % 100 < 10 || hour % 100 >= 20)) {
+      return 'часа';
+  } else {
+      return 'часов';
+  }
+}
 
-  const today = new Date();
-  const currentDay = today.getDay();
-  const currentDayIndex = currentDay === 0 ? 6 : currentDay - 1;
-  const weekDaysDiv = document.getElementById('weekDays');
+function formatTimeUnit(unit) {
+  return unit < 10 ? '0' + unit : unit;
+}
 
-  week.forEach((day, index) => {
-    const dayItem = document.createElement('p');
-    if (index === 5 || index === 6) {
-      dayItem.className = 'italic';
-    } else if (index === currentDayIndex) {
-      dayItem.className = 'bold';
-    }
-    dayItem.textContent = day;
-    weekDaysDiv.appendChild(dayItem);
-    
-  });
-});
+function displayDateTime() {
+  const now = new Date();
+  const weekday = now.toLocaleString('ru-RU', { weekday: 'long' });
+  const day = now.getDate();
+  const month = now.toLocaleString('ru-RU', { month: 'long' });
+  const year = now.getFullYear();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
 
+  const hourWord = getHourWord(hours);
+  const formattedDateA = `Сегодня ${weekday}, ${day} ${month} ${year} года, ${hours} ${hourWord} ${minutes} минут ${seconds} секунд`;
+  const formattedDateB = `${formatTimeUnit(day)}.${formatTimeUnit(now.getMonth() + 1)}.${year} - ${formatTimeUnit(hours)}:${formatTimeUnit(minutes)}:${formatTimeUnit(seconds)}`;
 
+  document.getElementById('date-time-a').innerText = formattedDateA;
+  document.getElementById('date-time-b').innerText = formattedDateB;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(displayDateTime, 1000);
+displayDateTime();
