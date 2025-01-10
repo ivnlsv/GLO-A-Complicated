2ce7da129f8bc73a66c3127b00687af9fcb211b1
ivnlsv <ivan.losev@denuo.legal>
2025-01-10 15:20:57 +0300

    updated

--- script.js
+++ script.js
@@ -1,34 +1,68 @@
-function getHourWord(hour) {
+function getWordHour(hour) {
   if (hour % 10 === 1 && hour % 100 !== 11) {
-      return 'час';
-  } else if ((hour % 10 >= 2 && hour % 10 <= 4) && (hour % 100 < 10 || hour % 100 >= 20)) {
-      return 'часа';
+    return "час";
+  } else if (
+    hour % 10 >= 2 &&
+    hour % 10 <= 4 &&
+    (hour % 100 < 10 || hour % 100 >= 20)
+  ) {
+    return "часа";
   } else {
-      return 'часов';
+    return "часов";
   }
 }
 
-function formatTimeUnit(unit) {
-  return unit < 10 ? '0' + unit : unit;
+function formatTime(item) {
+  return item < 10 ? "0" + item : item;
 }
 
-function displayDateTime() {
+function dateTime() {
   const now = new Date();
-  const weekday = now.toLocaleString('ru-RU', { weekday: 'long' });
+  const days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота",
+  ];
+  const months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+  ];
+  //const now = new Date();
+  //const weekday = now.toLocaleString('ru-RU', { weekday: 'long' });
+  const weekday = days[now.getDay()];
   const day = now.getDate();
-  const month = now.toLocaleString('ru-RU', { month: 'long' });
+  //const month = now.toLocaleString('ru-RU', { month: 'long' });
+  const month = months[now.getMonth()];
   const year = now.getFullYear();
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  const hourWord = getHourWord(hours);
-  const formattedDateA = `Сегодня ${weekday}, ${day} ${month} ${year} года, ${hours} ${hourWord} ${minutes} минут ${seconds} секунд`;
-  const formattedDateB = `${formatTimeUnit(day)}.${formatTimeUnit(now.getMonth() + 1)}.${year} - ${formatTimeUnit(hours)}:${formatTimeUnit(minutes)}:${formatTimeUnit(seconds)}`;
+  const wordHour = getWordHour(hours);
+  const longDate = `Сегодня ${weekday}, ${day} ${month} ${year} года, ${hours} ${wordHour} ${minutes} минут ${seconds} секунд`;
+  const shortDate = `${formatTime(day)}.${formatTime(
+    now.getMonth() + 1
+  )}.${year} - ${formatTime(hours)}:${formatTime(minutes)}:${formatTime(
+    seconds
+  )}`;
 
-  document.getElementById('date-time-a').innerText = formattedDateA;
-  document.getElementById('date-time-b').innerText = formattedDateB;
+  document.getElementById("date-time-full").innerText = longDate;
+  document.getElementById("date-time-short").innerText = shortDate;
 }
 
-setInterval(displayDateTime, 1000);
-displayDateTime();
+setInterval(dateTime, 1000);
+dateTime();
