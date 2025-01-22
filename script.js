aaef9df81b46756e5eb6b585d4b1ae20697c0268
ivnlsv <ivan.losev@denuo.legal>
2025-01-22 11:16:46 +0300

    upd

--- script.js
+++ script.js
@@ -5,57 +5,52 @@ function DomElement(selector, height, width, bg, fontSize) {
   this.bg = bg;
   this.fontSize = fontSize;
 
-  this.createElement = function(text) {
-      let element;
+  this.createElem = function (text) {
+    let elem;
 
-      if (this.selector.startsWith('.')) {
-          // Создаем div с классом
-          element = document.createElement('div');
-          element.className = this.selector.slice(1); // Убираем точку
-      } else if (this.selector.startsWith('#')) {
-          // Создаем параграф с id
-          element = document.createElement('p');
-          element.id = this.selector.slice(1); // Убираем решетку
-      }
+    if (this.selector.startsWith(".")) {
+      elem = document.createElement("div");
+      elem.className = this.selector.slice(1);
+    } else if (this.selector.startsWith("#")) {
+      elem = document.createElement("p");
+      elem.id = this.selector.slice(1);
+    }
 
-      element.style.cssText = `
+    elem.style.cssText = `
       height: ${this.height}px; 
       width: ${this.width}px; 
       background: ${this.bg}; 
       font-size: ${this.fontSize}px; 
       position: absolute;
   `;
-  element.textContent = text || 'Новый элемент';
-  document.body.appendChild(element);
-  return element;
-      }
- };
- document.addEventListener("DOMContentLoaded", function() {
-  const square = new DomElement('.block', 100, 100, 'lightblue', 16);
-  const element = square.createElement();
+    elem.textContent = text || "Новый элемент";
+    document.body.appendChild(elem);
+    return elem;
+  };
+}
+document.addEventListener("DOMContentLoaded", function () {
+  const square = new DomElement(".block", 100, 100, "lightblue", 16);
+  const element = square.createElem();
 
   let position = { x: 0, y: 0 };
 
-  function moveElement(event) {
-      switch (event.key) {
-          case 'ArrowUp':
-              position.y -= 10;
-              break;
-          case 'ArrowDown':
-              position.y += 10;
-              break;
-          case 'ArrowLeft':
-              position.x -= 10;
-              break;
-          case 'ArrowRight':
-              position.x += 10;
-              break;
-      }
-      element.style.transform = `translate(${position.x}px, ${position.y}px)`;
+  function moveElem(event) {
+    switch (event.key) {
+      case "ArrowUp":
+        position.y -= 10;
+        break;
+      case "ArrowDown":
+        position.y += 10;
+        break;
+      case "ArrowLeft":
+        position.x -= 10;
+        break;
+      case "ArrowRight":
+        position.x += 10;
+        break;
+    }
+    element.style.transform = `translate(${position.x}px, ${position.y}px)`;
   }
 
-  window.addEventListener('keydown', moveElement);
+  window.addEventListener("keydown", moveElem);
 });
-
-
-
