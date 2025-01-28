2553d38db8583a6f2661897f802abbdece8d5939
ivnlsv <ivan.losev@denuo.legal>
2025-01-28 10:35:45 +0300

    Updated

--- script.js
+++ script.js
@@ -1,56 +1,11 @@
-function DomElement(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
+const inputField = document.getElementById("textInput");
+const displayParagraph = document.getElementById("displayText");
+let timeoutId;
 
-  this.createElem = function (text) {
-    let elem;
+inputField.addEventListener("input", () => {
+  clearTimeout(timeoutId);
 
-    if (this.selector.startsWith(".")) {
-      elem = document.createElement("div");
-      elem.className = this.selector.slice(1);
-    } else if (this.selector.startsWith("#")) {
-      elem = document.createElement("p");
-      elem.id = this.selector.slice(1);
-    }
-
-    elem.style.cssText = `
-      height: ${this.height}px; 
-      width: ${this.width}px; 
-      background: ${this.bg}; 
-      font-size: ${this.fontSize}px; 
-      position: absolute;
-  `;
-    elem.textContent = text || "Новый элемент";
-    document.body.appendChild(elem);
-    return elem;
-  };
-}
-document.addEventListener("DOMContentLoaded", function () {
-  const square = new DomElement(".block", 100, 100, "lightblue", 16);
-  const element = square.createElem();
-
-  let position = { x: 0, y: 0 };
-
-  function moveElem(event) {
-    switch (event.key) {
-      case "ArrowUp":
-        position.y -= 10;
-        break;
-      case "ArrowDown":
-        position.y += 10;
-        break;
-      case "ArrowLeft":
-        position.x -= 10;
-        break;
-      case "ArrowRight":
-        position.x += 10;
-        break;
-    }
-    element.style.transform = `translate(${position.x}px, ${position.y}px)`;
-  }
-
-  window.addEventListener("keydown", moveElem);
+  timeoutId = setTimeout(() => {
+    displayParagraph.textContent = inputField.value;
+  }, 300);
 });
